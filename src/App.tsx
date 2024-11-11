0a1c62f7d2ac3b753265f71190e4ef622398918e
Nadia <nlnadialigia@gmail.com>
2024-11-11 19:25:49 -0300

    funcionalidades de criar, completar e excluir task

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,56 @@
+import { CirclePlus } from 'lucide-react';
+import { useState } from 'react';
 import './app.css';
-import { CreateTask } from './components/CreateTask';
 import { Header } from './components/Header';
-import { HeaderList, TasksList } from './components/List';
+import { List } from './components/List';
+
+export interface Task {
+  id: number;
+  title: string;
+  isDone: boolean;
+}
 
 function App() {
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [newTask, setNewTask] = useState<string>('');
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNewTask(e.target.value);
+  };
+
+  const handleAddTask = () => {
+    if (newTask.trim()) {
+      const newId = tasks.length + 1;
+
+      const newAdd: Task = {
+        id: newId,
+        title: newTask,
+        isDone: false,
+      };
+      setTasks([...tasks, newAdd]);
+      setNewTask('');
+    }
+  };
+
   return (
     <div className="app">
       <Header />
 
       <main>
         <div className="create">
-          <CreateTask />
+          <input
+            type="text"
+            placeholder="Adicionar uma nova tarefa"
+            value={newTask}
+            onChange={handleInputChange}
+          />
+          <button type="submit" onClick={handleAddTask}>
+            <span>Criar</span>
+            <CirclePlus />
+          </button>
         </div>
 
-        <div className="tasks">
-          <HeaderList />
-          <TasksList />
-        </div>
+        <List tasks={tasks} setTasks={setTasks} />
       </main>
     </div>
   );
--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,5 +1,68 @@
+import { Circle, CircleCheck, Trash2 } from 'lucide-react';
+import { Task } from '../../App';
 import { EmptyList } from './EmptyList';
-import { HeaderList } from './HeaderList';
-import { TasksList } from './TaskList';
 
-export { EmptyList, HeaderList, TasksList };
+interface ListProps {
+  tasks: Task[];
+  setTasks: React.Dispatch<React.SetStateAction<Task[]>>;
+}
+
+export function List({ tasks, setTasks }: Readonly<ListProps>) {
+  const hasTasks = tasks.length > 0;
+  const doneTasks = tasks.filter((task) => task.isDone);
+
+  const handleDoneTask = (id: number) => {
+    const updateTask = tasks.map((t) => {
+      if (t.id === id) {
+        return {
+          ...t,
+          isDone: true,
+        };
+      }
+      return t;
+    });
+    setTasks(updateTask);
+  };
+
+  const handleDeleteTask = (id: number) => {
+    const updateTask = tasks.filter((t) => t.id !== id);
+    setTasks(updateTask);
+  };
+
+  return (
+    <div className="tasks">
+      <div className="tasks-info">
+        <div className="common tasks-created">
+          <p>Tarefas criadas</p>
+          <span>{tasks.length}</span>
+        </div>
+        <div className=" common tasks-completed">
+          <p>Concluídas</p>
+          <span>{hasTasks ? `0 de ${doneTasks.length}` : 0}</span>
+        </div>
+      </div>
+
+      <div className="tasks-list">
+        {hasTasks ? (
+          tasks.map((task) => (
+            <div className="task" key={task.id}>
+              <button type="button" onClick={() => handleDoneTask(task.id)}>
+                {task.isDone ? (
+                  <CircleCheck color="#8284fa" />
+                ) : (
+                  <Circle color="#4EA8D3" />
+                )}
+              </button>
+              <p className={task.isDone ? 'done' : ''}>{task.title}</p>
+              <button type="button" onClick={() => handleDeleteTask(task.id)}>
+                <Trash2 />
+              </button>
+            </div>
+          ))
+        ) : (
+          <EmptyList />
+        )}
+      </div>
+    </div>
+  );
+}
